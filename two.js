96990167469ff413f8f7a7612a34036ffad62cf7
fahimowsame <144921853+fahimowsame@users.noreply.github.com>
2023-12-19 01:34:12 +0530

    Delete two.js

--- two.js
+++
@@ -1,9 +0,0 @@
-let username = "admin";
-let password = "123456";
-
-if (username === "admin" && password === "123456") {
-    console.log ("login sucessfull")
-}
-else {
-    console.log ("invalid credentials")
-}
